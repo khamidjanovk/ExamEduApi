ed4fc641af18c452a430b74f52ea3fbafb1d1ecc
Javlonbek Rajabov <radjabov377@mail.ru>
2022-04-04 10:20:20 +0500

    Filled controllers

--- MyEdu/Controllers/LessonsController.cs
+++ MyEdu/Controllers/LessonsController.cs
@@ -1,5 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MyEdu.Domain.Common;
+using MyEdu.Domain.Configurations;
+using MyEdu.Domain.Entities.Lessons;
+using MyEdu.Service.DTOs;
+using MyEdu.Service.Interfaces;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MyEdu.Controllers
 {
@@ -7,5 +15,53 @@ namespace MyEdu.Controllers
     [ApiController]
     public class LessonsController : ControllerBase
     {
+        private readonly ILessonService lessonService;
+        private readonly IWebHostEnvironment env;
+
+        public LessonsController(ILessonService lessonService, IWebHostEnvironment env)
+        {
+            this.lessonService = lessonService;
+            this.env = env;
+        }
+
+        [HttpPost]
+        public async ValueTask<ActionResult<BaseResponse<Lesson>>> PostCourseAsync([FromForm] LessonDto lessonDto)
+        {
+            var result = await lessonService.CreateAsync(lessonDto);
+
+            return StatusCode(result.Code ?? result.Error.Code.Value, result);
+        }
+
+        [HttpGet]
+        public async ValueTask<ActionResult<BaseResponse<IQueryable<Lesson>>>> GetCoursesAsync([FromQuery] PaginationParams @params)
+        {
+            var result = await lessonService.GetAllAsync(@params);
+
+            return StatusCode(result.Code ?? result.Error.Code.Value, result);
+        }
+
+        [HttpGet("{id}")]
+        public async ValueTask<ActionResult<BaseResponse<Lesson>>> GetCourseAsync(long id)
+        {
+            var result = await lessonService.GetAsync(p => p.Id == id);
+
+            return StatusCode(result.Code ?? result.Error.Code.Value, result);
+        }
+
+        [HttpPut("{id}")]
+        public async ValueTask<ActionResult<BaseResponse<Lesson>>> PutCourseAsync(long id, LessonDto lessonDto)
+        {
+            var result = await lessonService.UpdateAsync(id, lessonDto);
+
+            return StatusCode(result.Code ?? result.Error.Code.Value, result);
+        }
+
+        [HttpDelete("{id}")]
+        public async ValueTask<ActionResult<BaseResponse<Lesson>>> DeleteCourseAsync(long id)
+        {
+            var result = await lessonService.DeleteAsync(p => p.Id == id);
+
+            return StatusCode(result.Code ?? result.Error.Code.Value, result);
+        }
     }
 }
--- MyEdu/Controllers/UsersController.cs
+++ MyEdu/Controllers/UsersController.cs
@@ -1,5 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MyEdu.Domain.Common;
+using MyEdu.Domain.Configurations;
+using MyEdu.Domain.Entities.Users;
+using MyEdu.Service.DTOs;
+using MyEdu.Service.Interfaces;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MyEdu.Controllers
 {
@@ -7,6 +15,53 @@ namespace MyEdu.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
-        
+        private readonly IUserService userService;
+        private readonly IWebHostEnvironment env;
+
+        public UsersController(IUserService userService, IWebHostEnvironment env)
+        {
+            this.userService = userService;
+            this.env = env;
+        }
+
+        [HttpPost]
+        public async ValueTask<ActionResult<BaseResponse<User>>> PostCourseAsync([FromForm] UserDto userDto)
+        {
+            var user = await userService.CreateAsync(userDto);
+
+            return StatusCode(user.Code ?? user.Error.Code.Value, user);
+        }
+
+        [HttpGet]
+        public async ValueTask<ActionResult<BaseResponse<IQueryable<User>>>> GetCoursesAsync([FromQuery] PaginationParams @params)
+        {
+            var user = await userService.GetAllAsync(@params);
+
+            return StatusCode(user.Code ?? user.Error.Code.Value, user);
+        }
+
+        [HttpGet("{id}")]
+        public async ValueTask<ActionResult<BaseResponse<User>>> GetCourseAsync(long id)
+        {
+            var user = await userService.GetAsync(p => p.Id == id);
+
+            return StatusCode(user.Code ?? user.Error.Code.Value, user);
+        }
+
+        [HttpPut("{id}")]
+        public async ValueTask<ActionResult<BaseResponse<User>>> PutCourseAsync(long id, UserDto userDto)
+        {
+            var user = await userService.UpdateAsync(id, userDto);
+
+            return StatusCode(user.Code ?? user.Error.Code.Value, user);
+        }
+
+        [HttpDelete("{id}")]
+        public async ValueTask<ActionResult<BaseResponse<User>>> DeleteCourseAsync(long id)
+        {
+            var user = await userService.DeleteAsync(p => p.Id == id);
+
+            return StatusCode(user.Code ?? user.Error.Code.Value, user);
+        }
     }
 }
